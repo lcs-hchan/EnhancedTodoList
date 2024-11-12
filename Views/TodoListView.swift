7ccdf4365396f373e99cdfc41e2722a69e2a4b72
Hayden Chan <hchan@lcs.on.ca>
2024-11-12 11:29:27 -0500

    Added tab view

---
+++ Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  TodoListView.swift
+//  EnhancedTodoList
+//
+//  Created by hayden on 2024-11-12.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
