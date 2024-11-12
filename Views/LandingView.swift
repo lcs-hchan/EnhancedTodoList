29a8037bf94faed1ded9b08526b5d802aa1ebdc4
Hayden Chan <hchan@lcs.on.ca>
2024-11-12 11:27:42 -0500

    Added basic changes for the To-Do App

---
+++ Views/LandingView.swift
@@ -0,0 +1,18 @@
+//
+//  LandingView.swift
+//  EnhancedTodoList
+//
+//  Created by hayden on 2024-11-12.
+//
+
+import SwiftUI
+
+struct LandingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandingView()
+}
