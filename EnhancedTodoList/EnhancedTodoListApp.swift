8ce544cad6497c00c0b11d6f4e17f0647a66ed95
Hayden Chan <hchan@lcs.on.ca>
2024-11-12 11:22:46 -0500

    Initial Commit

---
+++ EnhancedTodoList/EnhancedTodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnhancedTodoListApp.swift
+//  EnhancedTodoList
+//
+//  Created by hayden on 2024-11-12.
+//
+
+import SwiftUI
+
+@main
+struct EnhancedTodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
